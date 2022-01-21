048f00ee4c7fc5c7a5996c19b86ebd9c677e1979
raaviabhiram <97377346+raaviabhiram@users.noreply.github.com>
2022-01-21 15:01:57 +0530

    Add files via upload

---
+++ RightAngleTriangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ 	int rows,i,j;
+ 	printf("number of rows :\n");
+ 	scanf("%d",&rows);
+ 	for(i=1;i<=rows;i++)
+ 	{
+ 		for(j=1;j<=i;j++)
+ 		{
+ 			printf("*\t");
+		}
+		printf("\n");
+	}
+	return 0;
+}
